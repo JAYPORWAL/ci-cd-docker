aade77c2da8cae2da0504fc0ef716f027d369016
JAYPORWAL <jayporwal3101@gmail.com>
2025-06-18 00:35:26 +0530

    code added new

---
+++ scr/app.js
@@ -0,0 +1,33 @@
+// src/App.js
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [user, setUser] = useState({ username: "", password: "" });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    alert(`Welcome ${user.username}`);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>React Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
