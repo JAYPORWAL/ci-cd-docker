7470f2b07e8cf73f52aa2cd20414a1b7bff7b0aa
JAYPORWAL <jayporwal3101@gmail.com>
2025-06-18 00:36:22 +0530

    code added

--- scr/app.js
+++ scr/app.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState } from "react";
-import "./App.css";
+import "./app.css";
 
 function App() {
   const [user, setUser] = useState({ username: "", password: "" });
